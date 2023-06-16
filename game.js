159e1780c45a7356e466fd2f3078e4117a5c3f0b
Cihan Mabocoglu <cihanmabocoglu@gmail.com>
2023-06-15 21:10:26 -0400

    fixed dataset.framework

--- game.js
+++ game.js
@@ -7,11 +7,15 @@ function flipCard(){
     this.classList.add('flip')
     if (!hasFlippedCard){
     //    first click
-        hasFlippedCard = true
+        hasFlippedCard = true;
+        firstCard = this;
     } else {
      // first click
         hasFlippedCard = false;
         secondCard = this;
+        
+        console.log(firstCard.dataset.framework);
+        console.log(secondCard.dataset.framework);
     }
 }
 cards.forEach (card => card.addEventListener('click',flipCard))
