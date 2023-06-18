2638eba7f19b90096c4492728d0b386ff5d9bccb
Cihan Mabocoglu <cihanmabocoglu@gmail.com>
2023-06-18 15:16:16 -0400

    bug fixes and added for functions

--- game.js
+++ game.js
@@ -1,22 +1,25 @@
 let cards = document.querySelectorAll('.cats')
-let hasFlippedCard = false 
+let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 
 function flipCard(){
-    this.classList.add('flip')
+    if(lockBoard) return;
+   if (this === firstCard) return;
+    
+   this.classList.add('flip')
     if (!hasFlippedCard){
     //    first click
         hasFlippedCard = true;
         firstCard = this;
-    } else {
-     // first click
-        hasFlippedCard = false;
+    return;
+    }
         secondCard = this;
         checkForMatch();
        }
     
-}
+
     // removed a lot of nested code and cleaned it up a bit
 
 function checkForMatch(){
@@ -29,15 +32,32 @@ function checkForMatch(){
 function cancelCards(){
      firstCard.removeEventListener('click', flipCard)
         secondCard.removeEventListener('click', flipCard)
+        resetBoard();
 }
 
 
 function unflipCards(){
+    lockBoard = true
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip'); 
+        resetBoard();
         },1500)
 }
+
+function resetBoard(){
+    [hasFlippedCard,lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffleCards() {
+    cards.forEach(card => {
+     let randomPos = Math.floor(Math.random() * 12);
+     card.style.order = randomPos;   
+    });
+    // IIFE function
+ })();  
+
 
 
 cards.forEach (card => card.addEventListener('click',flipCard))
