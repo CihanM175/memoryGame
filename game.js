a9bf4de56b3f3125b344673c6626cfc00d35117a
Cihan Mabocoglu <cihanmabocoglu@gmail.com>
2023-06-18 14:56:22 -0400

    removed a lot of nested code to look cleaner

--- game.js
+++ game.js
@@ -13,9 +13,31 @@ function flipCard(){
      // first click
         hasFlippedCard = false;
         secondCard = this;
-        
-        console.log(firstCard.dataset.framework);
-        console.log(secondCard.dataset.framework);
-    }
+        checkForMatch();
+       }
+    
 }
+    // removed a lot of nested code and cleaned it up a bit
+
+function checkForMatch(){
+    let isMatch = firstCard.dataset.framework === 
+    secondCard.dataset.framework;
+    
+    isMatch ? cancelCards() : unflipCards()
+}
+
+function cancelCards(){
+     firstCard.removeEventListener('click', flipCard)
+        secondCard.removeEventListener('click', flipCard)
+}
+
+
+function unflipCards(){
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip'); 
+        },1500)
+}
+
+
 cards.forEach (card => card.addEventListener('click',flipCard))
