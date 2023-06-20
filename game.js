92a80cebae07daef53c4431810d6edd58324bc5a
Cihan Mabocoglu <cihanmabocoglu@gmail.com>
2023-06-20 18:53:18 -0400

    end game and restart button stylize

--- game.js
+++ game.js
@@ -26,8 +26,10 @@ function checkForMatch(){
     let isMatch = firstCard.dataset.framework === 
     secondCard.dataset.framework;
     
-    isMatch ? cancelCards() : unflipCards()
+    isMatch ? cancelCards() : unflipCards();
 }
+   
+    
 
 function cancelCards(){
      firstCard.removeEventListener('click', flipCard)
@@ -42,7 +44,7 @@ function unflipCards(){
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip'); 
         resetBoard();
-        },1500)
+        },900)
 }
 
 function resetBoard(){
@@ -57,6 +59,110 @@ function resetBoard(){
     });
     // IIFE function
  })();  
+
+// Declare global variables
+let timerInterval;
+let seconds = 0;
+let timerDuration = 30; // Set the timer duration in seconds
+const timerElement = document.getElementById('timer');
+const startButton = document.getElementById('startButton');
+
+// Function to start the timer
+function startTimer() {
+  timerInterval = setInterval(updateTimer, 1000);
+}
+
+// Function to update the timer
+function updateTimer() {
+  seconds++;
+  timerElement.textContent = formatTime(timerDuration - seconds);
+  if (seconds >= timerDuration) {
+    stopTimer();
+    gameOver ();
+  }
+}
+
+// Function to format the time values as two digits
+function formatTime(time) {
+  return time < 10 ? '0' + time : time;
+}
+
+// Function to stop the timer
+function stopTimer() {
+  clearInterval(timerInterval);
+}
+
+// Event listener for the start button
+startButton.addEventListener('click', function() {
+    startTimer();
+    startButton.style.display = 'none'; // Hide the button after starting the game
+  });
+
+
+  // Get the game over modal and restart button elements
+let modal = document.getElementById('game-over-modal');
+let restartButton = document.getElementById('restartButton');
+
+// Function to display the game over modal
+function showGameOverModal() {
+  modal.style.display = 'flex';
+}
+
+// Function to hide the game over modal
+function hideGameOverModal() {
+  modal.style.display = 'none';
+}
+function gameOver(){
+    showGameOverModal();
+}
+
+function resetGameBoard() {
+    cards.forEach((card) => {
+    card.removeEventListener('click', flipCard);
+      card.classList.remove('flip');
+    });
+  
+    setTimeout(() => {
+      cards.forEach((card) => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+      });
+      cards.forEach((card) => {
+        card.addEventListener('click', flipCard);
+      });
+      lockBoard = false; // Reset the lockBoard flag
+    }, 1000); // Delay the shuffling to allow cards to flip back
+  }
+
+  function resetGame() {
+    stopTimer();
+    seconds = 0;
+    timerElement.textContent = formatTime(timerDuration);
+    hideGameOverModal();
+    resetGameBoard();
+    startTimer();
+  }
+  
+
+// Add an event listener to the restart button
+restartButton.addEventListener('click', function() {
+    hideGameOverModal();
+   resetGame();
+  });
+// You win modal
+  const youWinModal = document.getElementById('you-win-modal');
+
+function showYouWinModal() {
+  youWinModal.style.display = 'flex';
+}
+function hideYouWinModal() {
+    youWinModal.style.display = 'none';
+  }
+  
+
+
+
+
 
 
 
